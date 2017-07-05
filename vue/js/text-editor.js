59f41af2973040fb0d438d7bf366dbac629530e7
lyes <lyes.amd@gmail.com>
2017-07-05 14:09:26 +0200

    Ajout editeur de texte

---
+++ vue/js/text-editor.js
@@ -0,0 +1,42 @@
+
+        function iFrameOn(){
+            richTextField.document.designMode = 'On';
+        }
+        function iBold(){
+            richTextField.document.execCommand('bold',false,null); 
+        }
+        function iUnderline(){
+            richTextField.document.execCommand('underline',false,null);
+        }
+        function iItalic(){
+            richTextField.document.execCommand('italic',false,null); 
+        }
+        function iFontSize(){
+            var size = prompt('Entrez une valeur entre 1 et 7 -- Autrement, la taille ne changera pas', '');
+            richTextField.document.execCommand('FontSize',false,size);
+        }
+        function iForeColor(){
+            var color = prompt('Donnez un code couleur en hexadecimal', '#');
+            richTextField.document.execCommand('ForeColor',false,color);
+        }
+        function iHorizontalRule(){
+            richTextField.document.execCommand('inserthorizontalrule',false,null);
+        }
+        function iUnorderedList(){
+            richTextField.document.execCommand("InsertOrderedList", false,"newOL");
+        }
+        function iOrderedList(){
+            richTextField.document.execCommand("InsertUnorderedList", false,"newUL");
+        }
+        function iLink(){
+            var linkURL = prompt("Entrer l'URL du lien :", "http://"); 
+            richTextField.document.execCommand("CreateLink", false, linkURL);
+        }
+        function iUnLink(){
+            richTextField.document.execCommand("Unlink", false, null);
+        }
+        function submit_form(){
+            var theForm = document.getElementById("textEditor");
+            theForm.elements["zone-saisie"].value = window.frames['richTextField'].document.body.innerHTML;
+            theForm.submit();
+        }
--- vue/new-publication.php
+++ vue/new-publication.php
@@ -9,12 +9,11 @@
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="css/bootstrap.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="css/wiki-style.css">
-    <link href="../vue/css/bootstrap.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="../vue/css/wiki-style.css">
+    <script type="text/javascript" src="js/text-editor.js"></script>
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 </head>
 
-<body>
+<body onLoad="iFrameOn();">
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
             <div class="navbar-header">
@@ -72,34 +71,41 @@
     </nav>
 
     <section class="container-wiki-text-editor">
-        <form action="../Controller/Controller.php?page=ajoutMedia" method="post" enctype="multipart/form-data">
-            <p>
-                Formulaire d'envoi de fichier :<br />
-                <input type="file" name="media" /><br />
-                <input type="submit" value="Envoyer le fichier" />
-            </p>
-        </form>
-        <form class="" action="../Controller/UserController.php?page=ajoutPublication" autocomplete="on" method="POST">
-
-            <div class="input-group">
-                <span class="input-group-addon"><i class=""></i></span>
-                <input type="text" size="25" class="form-control " name="" placeholder="" autofocus autocomplete="on" required>
+        <h2 class="title-new-publication">Création d'une nouvelle publication</h2><br>
+        <form action="../Controller/textEditor.php" name="textEditor" id="textEditor" method="post">
+            <div class="form-group">
+                <label for="publicationTitle">Titre de votre publication</label>
+                <input type="text" class="form-control" id="publicationTitle" placeholder="EX: quelque chose blabla">
             </div>
-            <span class="help-block"></span>
+            <br>
+            <div class="form-group">
+                <label for="zone-text">Mise en texte</label>
+                <div id="zone-text form-group">
+                  <input class="btn" type="button" onClick="iBold()" value="B">
+                  <input class="btn" type="button" onClick="iUnderline()" value="U">
+                  <input class="btn" type="button" onClick="iItalic()" value="I">
+                  <input class="btn" type="button" onClick="iFontSize()" value="Text Size">
+                  <input class="btn" type="button" onClick="iForeColor()" value="Text Color">
+                  <input class="btn" type="button" onClick="iHorizontalRule()" value="HR"> 
+                  <input class="btn" type="button" onClick="iUnorderedList()" value="UL">
+                  <input class="btn" type="button" onClick="iOrderedList()" value="OL">
+                  <input class="btn" type="button" onClick="iLink()" value="Link">
+                  <input class="btn" type="button" onClick="iUnLink()" value="UnLink">
 
-            <?php
-            echo "<select id='select'>";
-            foreach ($listeThematique as $thematique) {
-                    echo "<option value=".$thematique['id_thematique'].">".$thematique['nom']."</option>";
-            }
-            echo "</select>";
-            ?>
-            <div class="input-group">
-                <span class="input-group-addon"><i class=""></i></span>
-                <textarea name="" id =""></textarea>
+                </div><br><br>
+                <!-- Hide(but keep)your normal textarea and place in the iFrame replacement for it -->
+                <textarea style="display:none;" name="zone-saisie" id="zone-saisie" cols="100" rows="14"></textarea>
+                <iframe name="richTextField" id="richTextField" style="border:#000000 1px solid; width:100%; height:300px;"></iframe>
+                <!-- End replacing your normal textarea -->
             </div>
-
-            <button class="btn btn-lg btn-success btn-block" type="submit">Ajout publication</button>
+            <br>
+            <div class="form-group">
+                <label for="fileUpload">Charger un fichier (optionnel)</label>
+                <input type="file" id="fileUpload">
+                <p class="help-block">Formats acceptés : mp3, jpeg</p>
+            </div>
+            <br>
+            <input class="btn btn-primary" name="btnSubmit" type="button" value="Publier" onClick="javascript:submit_form();"/>
         </form>
     </section>
 </body>
