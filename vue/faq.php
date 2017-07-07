b5e084104133dedad7b3f296b005364474e285bd
lyes <lyes.amd@gmail.com>
2017-07-07 11:25:50 +0200

    Des modifs dans la faq

--- vue/faq.php
+++ vue/faq.php
@@ -58,15 +58,15 @@
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             <h4 class="panel-title">
-                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseFour">Qui a réalisé cette plateforme?</a>
+                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseFour">Qui a réalisé cette plateforme ?</a>
                             </h4>
                         </div>
                         <div id="collapseFour" class="panel-collapse collapse">
                             <div class="panel-body">
                                 Par 3 étudiants de l'école HiTema : 
+                                    <ol>Khaled El Ghandri</ol>
                                     <ol>Lyes Ameddah</ol>
                                     <ol>Jason Nicolaï</ol>
-                                    <ol>Khaled El Ghandouri</ol>
                             </div>
                         </div>
                     </div>
